63cafd671956ad4c1e9e7cf01f17300172c260db
justin <justin@elloro.nl>
2024-03-01 07:32:15 +0100

    Basic styling

--- src/views/Cv.vue
+++ src/views/Cv.vue
@@ -1,3 +1,11 @@
 <template>
-  CV here
+  <div class="marquee-container">
+    <div class="marquee page-title">
+      <span>Curriculum Vitae<span class="star">✦</span></span>
+      <span>Curriculum Vitae<span class="star">✦</span></span>
+      <span>Curriculum Vitae<span class="star">✦</span></span>
+      <span>Curriculum Vitae<span class="star">✦</span></span>
+    </div>
+  </div>
+  <div class="container">CV here</div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,30 @@
 <template>
 	<section class="home-header">
-    <div class="marquee">
-      <span>Justin Picard&nbsp;✦&nbsp;Digital Product Designer</span>
-      <span>Justin Picard&nbsp;✦&nbsp;Digital Product Designer</span>
-      <span>Justin Picard&nbsp;✦&nbsp;Digital Product Designer</span>
-      <span>Justin Picard&nbsp;✦&nbsp;Digital Product Designer</span>
-    </div>
-		<div class="container">
-			<h2>Hi 👋🏼 my name is Justin Picard and I'm a digital product designer. I love designing applications, platforms and websites that look great and are easy to use. For the last 10 years I have spent my time developing my design- and coding skills at <a href="#">a bunch of digital agencies</a>.</h2>
-			<p class="text">no 🐂💩</p>
-		</div>
-		<div class="personal-image">
-      <div class="container">
-			  <img :src="getImageUrl('justin-picard','jpg')" alt="">
+    <div class="marquee-container">
+      <div class="marquee name">
+        <h2>Justin Picard<span class="star">✦</span></h2>
+        <h2>Justin Picard<span class="star">✦</span></h2>
+        <h2>Justin Picard<span class="star">✦</span></h2>
+        <h2>Justin Picard<span class="star">✦</span></h2>
       </div>
-		</div>
+      <div class="marquee job-title">
+        <h2>Digital Product Designer<span class="star">✦</span></h2>
+        <h2>Digital Product Designer<span class="star">✦</span></h2>
+        <h2>Digital Product Designer<span class="star">✦</span></h2>
+        <h2>Digital Product Designer<span class="star">✦</span></h2>
+      </div>
+    </div>
+    <div class="personal-image">
+      <figure>
+        <img :src="getImageUrl('justin-picard','jpg')" alt="">
+      </figure>
+    </div>
 	</section>
 	<section class="bio">
+    <div class="container">
+      <h2>Hi 👋🏼 my name is Justin Picard and I'm a digital product designer. I love designing applications, platforms and websites that look great and are easy to use. For the last 10 years I have spent my time developing my design- and coding skills at <a href="#">a bunch of digital agencies</a>.</h2>
+      <p class="text">no 🐂💩</p>
+    </div>
 		<div class="">
 			<h3>A little bit more about me</h3>
 
