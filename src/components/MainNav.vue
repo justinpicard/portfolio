8bb22535ce39e79ca5d5cb257e691dec79202335
Justin Picard <hallo@justinpicard.nl>
2024-03-01 07:39:05 +0100

    Add new main nav

---
+++ src/components/MainNav.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="above-pages position-relative">
+		<div class="position-fixed top-0 start-0 ms-3 mt-2">
+			<a href="header-logo">Justin Picard</a>
+		</div>
+		<div class="position-fixed top-0 end-0 me-3 mt-2">
+			<button type="button" class="btn btn-outline-primary btn-circle btn-toggle">☾</button>
+		</div>
+		<div class="position-fixed bottom-0 start-0 ms-3 mb-2">
+			About
+		</div>
+		<div class="position-fixed bottom-0 end-0 me-3 mb-2">
+			Contact
+		</div>
+	</div>
+</template>
